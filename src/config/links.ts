47a8cd3b8df3489c6741a7a43d32715e535e9c15
yxlr233 <121668149+yxlr233@users.noreply.github.com>
2026-05-04 11:04:36 +0800

    Update link.ts

--- src/config/links.ts
+++ src/config/links.ts
@@ -5,10 +5,4 @@ export const links = [
     description: "The web framework for content-driven websites",
     avatar: "/links/astro.svg",
   },
-  {
-    name: "Guoqi Sun",
-    link: "https://blog.sunguoqi.com",
-    description: "Try, fail, retry. That's the rhythm of growth.",
-    avatar: "https://assets.guoqi.dev/images/avatar.png",
-  },
 ]
